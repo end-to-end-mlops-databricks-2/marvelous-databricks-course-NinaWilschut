4ec3e8286cb75e4cddfcb1821209fa05951ca505
Azure Devops pipelines <cicd@net.stedin>
2025-03-10 12:06:14 +0100

    update code and serving

--- scripts/01_process_data.py
+++ scripts/01_process_data.py
@@ -5,7 +5,7 @@ import yaml
 from pyspark.sql import SparkSession
 
 from power_consumption.config import ProjectConfig
-from power_consumption.data_processor import DataProcessor, generate_synthetic_data
+from power_consumption.data_preprocessing import DataProcessor, generate_synthetic_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
